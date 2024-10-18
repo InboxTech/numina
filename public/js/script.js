6e05137e2dfd8cb34e51a2f83fabb63c15a27b1f
krishna sheth <krishna.sheth@inbox-infotech.com>
2024-10-18 09:47:52 +0530

    index page designed.

---
+++ public/js/script.js
@@ -0,0 +1,71 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".myMagazineSwiper", {
+    // slidesPerView: 1,
+    spaceBetween: 2,
+    centeredSlides: true,
+    pagination: {
+      // el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 5,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".myLifeSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      // 640: {
+      //   slidesPerView: 2,
+      //   spaceBetween: 10,
+      // },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
