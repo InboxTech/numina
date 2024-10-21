f511329fd43383836089ef119e15366110c9c0a7
krishna sheth <krishna.sheth@inbox-infotech.com>
2024-10-21 11:39:29 +0530

    header responsive

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,22 @@
-var submenu_pro = document.getElementById("pro-submenu"); 
-var show_submenu_pro = document.getElementById("show-pro-submenu");
+// var submenu_pro = document.getElementById("pro-submenu");
+// var show_submenu_pro = document.getElementById("show-pro-submenu");
 
-// Variable to keep track of the menu visibility
-var isVisible = false;
+// // Variable to keep track of the menu visibility
+// var isVisible = false;
 
-submenu_pro.addEventListener("click", function() {
-    // Toggle between show and hide on a single click
-    if (!isVisible) {
-        show_submenu_pro.style.display = 'flex';
-        isVisible = true;
-    } else {
-        show_submenu_pro.style.display = 'none';
-        isVisible = false;
-    }
-});
+// submenu_pro.addEventListener("click", function() {
+//     // Toggle between show and hide on a single click
+//     if (!isVisible) {
+//         show_submenu_pro.style.display = 'flex';
+//         isVisible = true;
+//     } else {
+//         show_submenu_pro.style.display = 'none';
+//         isVisible = false;
+//     }
+// });
 
 // Optional: If you still want to add a double-click event
-submenu_pro.addEventListener("dblclick", function() {
-    show_submenu_pro.style.display = 'none';
-    isVisible = false; // Ensure the visibility state is reset
-});
+// submenu_pro.addEventListener("dblclick", function() {
+//     show_submenu_pro.style.display = 'none';
+//     isVisible = false; // Ensure the visibility state is reset
+// });
