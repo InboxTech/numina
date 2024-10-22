83f029f2b449d69678879d4be3024a0924a0462b
Aadil Shaikh <aadil@inbox-infotech.com>
2024-10-22 17:02:45 +0530

    add header in php

--- header.php
+++ header.php
@@ -1,57 +1,67 @@
 <?php
-    class header {
-        public function includeHeader($title,$path,){
-            echo '<!doctype html>
+class header
+{
+    public function includeHeader($title, $path,)
+    {
+        echo '<!doctype html>
 <html lang="en">
 
 <head>
-    <title>'.$title.'</title>
+    <title>' . $title . '</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <!-- links -->
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="'.$path.'public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="'.$path.'public/css/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="'.$path.'public/font/css/all.min.css">
-    <!-- <link rel="stylesheet" href="'.$path.'public/fonts/tabler-icons.css"> -->
-    <link rel="stylesheet" href="'.$path.'public/css/style.css">
+    <link rel="stylesheet" href="' . $path . 'public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="' . $path . 'public/css/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="' . $path . 'public/font/css/all.min.css">
+    <!-- <link rel="stylesheet" href="' . $path . 'public/fonts/tabler-icons.css"> -->
+    <link rel="stylesheet" href="' . $path . 'public/css/style.css">
 
 </head>
 
 <body>
     <header>
-        <div class="header container-fluid bg-black text-end p-2">
-            <a href="#" class="text-decoration-none fs-5 border-end pe-3 orange-link">
-                <i class="fa-regular fa-envelope"></i> Contact us
-            </a>
-            <a href="#" class="text-decoration-none fs-5 ms-3 orange-link">
-                <i class="fa-solid fa-magnifying-glass"></i> Search
-            </a>
-        </div>
-
-        <!----------------------------------------------Navbar-------------------------------------------------->
-
-        <nav class="navbar row border-bottom">
-            <div class="logo col-6">
-                <img src="public/images/header/Numina Logo.png" class="w-100 h-inherit" alt="" />
-            </div>
-            <div class="nav-links col-6">
-                <a href="#" class="text-decoration-none fs-5"> Who is Numina </a>
-                <a href="#" class="text-decoration-none fs-5" id="pro-submenu">
-                    Our product
-                </a>
-                <a href="#" class="text-decoration-none fs-5">
-                    Reasearch and development
-                </a>
-                <a href="#" class="text-decoration-none fs-5"> F & Q </a>
-                <a href="#" class="text-decoration-none fs-5"> Drop us a line </a>
+       <!----------------------------------------------Navbar-------------------------------------------------->
+        
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand logo" href="#"><img src="public/images/header/Numina Logo.png"
+                        class="w-100 h-inherit" alt="" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a href="aboutus.html" class="nav-link fs-5"> Who is Numina </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link fs-5 postion-relative" id="pro-submenu">
+                                Our product
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link fs-5">
+                                Reasearch and development
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link fs-5"> F & Q </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link fs-5"> Drop us a line </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
-        <div class="product-main-menu border rounded-top-0 rouded-bottom-5 p-5 container mx-auto row justify-content-evenly"
-            id="show-pro-submenu">
-            <div class="menu border-end col-4 row flex-column">
+
+        <div class="product-main-menu container row border rouded-bottom-5 mx-auto ps-5" id="show-pro-submenu">
+            <div class="menu col-lg-6 col-md-12 col-sm-12 border-lg-end p-3 row flex-column">
                 <a href="#" class="text-decoration-none fw-bold mb-3 fs-3 link-orange">
                     Product <span class="fs-2 right-arrow"> &#8594; </span>
                 </a>
@@ -93,75 +103,120 @@
                 </a>
             </div>
 
-            <!-- <div class="menu border-end ps-5 col-4 postion-relative">
-                <div class="d-none row flex-column" id="show-antibiotic-submenu">
-                    <a href="#" class="text-decoration-none fw-bold mb-3 fs-3 link-orange"> Outstanding people <span class="fs-2 right-arrow"> &#8594; </span> </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> AZITHROMYCIN TABLETS	 </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> CEFUROXIME AXETIL TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> CEFIXIME  TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation">	CEFIXIME  + OFLOXACIN TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> CEFIXIME  + POTASSIUM CLAVULANATE TABLETS  </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> CEPHALEXIN TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> AMOXYCILLIN TRIHYDRATE TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> AMOXYCILLIN TRIHYDRATE  + POTASSIUM CLAVULANATE TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> ETORICOXIB TAB LETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> LEVOFLOXACIN HEMIHYDRATE TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> AMPICILLIN TRIHYDRATE  + DICLOXACILLIN SODIUM TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> 	DOXYCYCLINE HYCLATE TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> LINEZOLID TABLETS  </a>
-                </div>
-
-                <div class="d-none row flex-column" id="show-anl-antipy-antiem-submenu">
-                    <a href="#" class="text-decoration-none fw-bold mb-3 fs-3 link-orange"> Outstanding people <span class="fs-2 right-arrow"> &#8594; </span> </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> PARACETAMOL + DOMPERIDONE MALEATE TABLETS	 </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> ACECLOFENAC & PARACETAMOL TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> ACECLOFENAC, PARACETAMOL & CHLORZOXAZONE TABLETS	</a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> ACECLOFENAC, PARACETAMOL & SERRATIOPEPTIDASE TABLETS	</a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> DICLOFENAC & PARACETAMOL TABLETS	  </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> DICLOFENAC POTASSIUM, PARACETAMOL, CHLORPHENIRAMINE MALEATE & MAGNESIUM TRISILICATE TABLETS	 </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> DICLOFENAC SODI. PARACETAMOL & CHLORZOXAZONE TABLETS	 </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> 	DICLOFENAC, PARACETAMOL AND SERATIOPEPTIDASE TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> DICLOFENAC, PARACETAMOL, MAGNISUM TRISILICATE TABLETS </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> NIMESULIDE & PARACETAMOL TABLETS	 </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> 	PARACETAMOL, DICLOFENAC & CHLORZOXAZONE TABLETS	 </a>
-                    
-                </div>
-
-                <div class="governance-submenu d-none row flex-column" id="show-gov-submenu">
-                    <a href="#" class="text-decoration-none fw-bold mb-3 fs-3 link-orange"> Governance <span class="fs-2 right-arrow"> &#8594; </span> </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> Committees </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> Shareholders  </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> Sarbanes-Oxley Act 20002 </a>
-                    <a href="#" class="text-decoration-none link-hover-animation"> Compliance </a>
-                </div>
-            </div> -->
-
-            <div class="menu col-4">
-                <div class="featured-section">
-                    <a href="#" class="text-decoration-none">
-                        <span class="text-uppercase"> featured section </span>
-                        <h3 class="fs-4 fw-light">Our purpose, strategy and culture</h3>
-
+            <div class="menu col-lg-6 col-md-12 col-sm-12">
+                <div class="details row justify-content-center align-items-center p-3">
+                    <div class="product-details">
+                        <h3 class="fs-4">Antacid</h3>
+                        <h4 class="fs-5">Relief in 60 Secs</h4>
+                        <p>Fast relief for heartburn & indigestion</p>
                         <div class="feature-img w-75 rounded-4">
                             <img src="public/images/header/gsk_tech_human_image_6k.jpg"
                                 class="w-100 h-inherit rounded-4" alt="" />
                         </div>
-                    </a>
-                </div>
+                    </div>
 
-                <div class="featured-section my-3">
-                    <a href="#" class="text-decoration-none">
-                        <span class="text-uppercase"> featured section </span>
-                        <h3 class="fs-4 fw-light">Outstanding people</h3>
-
+                    <div class="product-details">
+                        <h3 class="fs-4">Antibiotics</h3>
+                        <h4 class="fs-5">Solution for Every Infection</h4>
+                        <p>Targeted treatments for bacterial infections.</p>
                         <div class="feature-img w-75 rounded-4">
-                            <img src="public/images/header/sddefault-2.jpg" class="w-100 h-inherit rounded-4" alt="" />
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
                         </div>
-                    </a>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Analgesic</h3>
+                        <h4 class="fs-5">Pain Relief in 30 Mins</h4>
+                        <p>Fast-acting solutions to manage pain n</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Analgesic, Antipyretic, and Anti-emetic</h3>
+                        <h4 class="fs-5">Triple Action, 1 Pill</h4>
+                        <p>Pain, fever, and nausea relief in one drug</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Analgesic & Anti-inflammatory</h3>
+                        <h4 class="fs-5">Dual Action in 45 Minutes</h4>
+                        <p>Relieves pain and controls inflammation</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Anti-allergic</h3>
+                        <h4 class="fs-5">Breathe Easy in 5 Minutes</h4>
+                        <p>Rapid relief from allergy symptoms</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Multivitamins and Supplements</h3>
+                        <h4 class="fs-5">Daily Health Boost</h4>
+                        <p>Essential nutrients for overall wellness</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Anti-diabetics</h3>
+                        <h4 class="fs-5">1 Dose, Better Control</h4>
+                        <p>Effective management of blood sugar</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Cardiovascular Drugs</h3>
+                        <h4 class="fs-5">Heart Health Matters</h4>
+                        <p>Supports cardiovascular health quickly</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Antimicrobials</h3>
+                        <h4 class="fs-5">Defend in 10 Days</h4>
+                        <p>Protection against harmful microorganisms</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="product-details">
+                        <h3 class="fs-4">Antiretrovirals (ARVs)</h3>
+                        <h4 class="fs-5">Solution for Life</h4>
+                        <p>Effective management for healthier living with HIV</p>
+                        <div class="feature-img w-75 rounded-4">
+                            <img src="public/images/header/gsk_tech_human_image_6k.jpg"
+                                class="w-100 h-inherit rounded-4" alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </header>';
-        }
     }
-?>
+}
