1b7e93f881a4c028700cd4c3c45bd5731bd69c5c
Shaikh Aadilhusain <aadil@inbox-infotech.com>
2024-12-05 09:18:40 +0530

    auto swiper time set

--- public/js/script.js
+++ public/js/script.js
@@ -34,6 +34,112 @@ document.addEventListener("click", function(event) {
     }
 });
 
+//main page swiper
+var swiper = new Swiper(".section2Swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1.6,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 1.9,
+      spaceBetween: 50,
+    },
+    1200: {
+      slidesPerView: 2.5,
+      spaceBetween: 60,
+    },
+    1440: {
+      slidesPerView: 4,
+      // spaceBetween: 70,
+    },
+  },
+});
+
+var swiper = new Swiper(".section3Swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1.6,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 1.9,
+      spaceBetween: 50,
+    },
+    1200: {
+      slidesPerView: 2.5,
+      spaceBetween: 60,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 70,
+    },
+  },
+});
+
+var swiper = new Swiper(".section4Swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   type: "progressbar",
+  // },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+  },
+});
+
 // Handling hover events for showing product details
 var link_hover = document.querySelectorAll(".link-hover-animation");
 var product_details = document.querySelectorAll(".product-details");
